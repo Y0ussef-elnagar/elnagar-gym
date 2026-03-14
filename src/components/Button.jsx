36c836881d93a91203f8011c6f5219aec9cec8fe
Youssef Elnagar <Youssef@gmail.com>
2026-03-14 07:29:37 +0200

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ text, func }) => {
+    return (
+        <button
+            onClick={func}
+            className="px-8 py-4 mx-auto rounded-md border-2 bg-slate-950 border-blue-400 border-solid cursor-pointer blueShadow duration-200"
+        >
+            {" "}
+            <p>{text}</p>
+        </button>
+    );
+};
+
+export default Button;
